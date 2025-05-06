5a0475a6707a02f4194344089e5b59804118c43f
Toryn <torync03@gmail.com>
2025-05-06 00:55:51 -0400

    setup

---
+++ Linear Regression Health Cost Calculator.py
@@ -0,0 +1,15 @@
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+import tensorflow as tf
+
+from tensorflow import keras
+from tensorflow.keras import layers
+
+import tensorflow_docs as tfdocs
+import tensorflow_docs.plots
+import tensorflow_docs.modeling
+
+print("TensorFlow version:", tf.__version__)
